55cc3f41fa064c3cfd0a00a18f6a5bbc7ba10202
Nugrohodwi14 <62677519+Nugrohodwi14@users.noreply.github.com>
2022-07-30 12:39:56 +0700

    Add files via upload

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,116 +1,59 @@
-import { Card, Container, Row, Col, Image } from 'react-bootstrap';
-import duneImage from "../assets/images/trending/dune.jpg";
-import infiniteImage from "../assets/images/trending/infinite.jpg";
-import everythingImage from "../assets/images/trending/everything.jpg";
-import jokerImage from "../assets/images/trending/joker.jpg";
-import lightyearImage from "../assets/images/trending/lightyear.jpg";
-import morbiusImage from "../assets/images/trending/morbius.jpg";
+import { useEffect, useState } from "react"
+import { Card, Container, Row, Col, Image } from "react-bootstrap"
+import duneImage from "../assets/images/trending/dune.jpg"
+import everythingImage from "../assets/images/trending/everything.jpg"
+import infiniteImage from "../assets/images/trending/infinite.jpg"
+import jokerImage from "../assets/images/trending/joker.jpg"
+import lightyearImage from "../assets/images/trending/lightyear.jpg"
+import morbiusImage from "../assets/images/trending/morbius.jpg"
+import axios from "axios"
+
+
 
 const Trending = () => {
-    return (
-        <div>
-            <Container>
-                <br />
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BASE_URL}/discover/movie`, {
+      params: {
+        api_key: process.env.REACT_APP_TMDB_KEY
+      }
+    }).then((response) => {
+      setMovies(response.data.results)
+    })
+  }, [])
 
-                <div id='trending'>
-                    <h1 className='text-white text-center ' > TRENDING MOVIES </h1>
-                </div>
 
-                <br />
-                <Row>
-                    <Col md={4} className="moviesWrapper" >
-                        <Card className="bg-dark text-white movieImage p-2 m-1" >
-                            <Image
-                                src={duneImage}
-                                alt="Dune Movies"
-                                className="images"
-                            />
-                            <Card.Title className='text-center' > Dune </Card.Title>
-                            <Card.Text className='text-left' >
-                                This is a wider card with supporting text below as a natural lead-in to additional content.
-                            </Card.Text>
-                            <Card.Text className='text-left' >Last updated 3 mins ago</Card.Text>
-                        </Card>
-                    </Col>
-
-                    <Col md={4} className="moviesWrapper" >
-                        <Card className="bg-dark text-white movieImage p-2 m-1" >
-                            <Image
-                                src={infiniteImage}
-                                alt="Infinite Movies"
-                                className="images"
-                            />
-                            <Card.Title className='text-center' > Infinite </Card.Title>
-                            <Card.Text className='text-left' >
-                                This is a wider card with supporting text below as a natural lead-in to additional content.
-                            </Card.Text>
-                            <Card.Text className='text-left' >Last updated 3 mins ago</Card.Text>
-                        </Card>
-                    </Col>
-
-                    <Col md={4} className="moviesWrapper" >
-                        <Card className="bg-dark text-white movieImage p-2 m-1" >
-                            <Image
-                                src={everythingImage}
-                                alt="Everything Movies"
-                                className="images"
-                            />
-                            <Card.Title className='text-center' > Everything </Card.Title>
-                            <Card.Text className='text-left' >
-                                This is a wider card with supporting text below as a natural lead-in to additional content.
-                            </Card.Text>
-                            <Card.Text className='text-left' >Last updated 3 mins ago</Card.Text>
-                        </Card>
-                    </Col>
-
-                    <Col md={4} className="moviesWrapper" >
-                        <Card className="bg-dark text-white movieImage p-2 m-1" >
-                            <Image
-                                src={morbiusImage}
-                                alt="Morbius Movies"
-                                className="images"
-                            />
-                            <Card.Title className='text-center' > Morbius </Card.Title>
-                            <Card.Text className='text-left' >
-                                This is a wider card with supporting text below as a natural lead-in to additional content.
-                            </Card.Text>
-                            <Card.Text className='text-left' >Last updated 3 mins ago</Card.Text>
-                        </Card>
-                    </Col>
-
-                    <Col md={4} className="moviesWrapper" >
-                        <Card className="bg-dark text-white movieImage p-2 m-1" >
-                            <Image
-                                src={lightyearImage}
-                                alt="Lightyear Movies"
-                                className="images"
-                            />
-                            <Card.Title className='text-center' > Lightyear </Card.Title>
-                            <Card.Text className='text-left' >
-                                This is a wider card with supporting text below as a natural lead-in to additional content.
-                            </Card.Text>
-                            <Card.Text className='text-left' >Last updated 3 mins ago</Card.Text>
-                        </Card>
-                    </Col>
-
-                    <Col md={4} className="moviesWrapper" >
-                        <Card className="bg-dark text-white movieImage p-2 m-1" >
-                            <Image
-                                src={jokerImage}
-                                alt="Joker Movies"
-                                className="images"
-                            />
-                            <Card.Title className='text-center' > Joker </Card.Title>
-                            <Card.Text className='text-left' >
-                                This is a wider card with supporting text below as a natural lead-in to additional content.
-                            </Card.Text>
-                            <Card.Text className='text-left' >Last updated 3 mins ago</Card.Text>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    )
+  return (
+    <div>
+      <Container>
+        <br />
+        <h1 className="text-white">TRENDING MOVIES</h1>
+        <br />
+        <Row>
+          {movies.map((result, index) => {
+            return (
+              <Col md={4} className="movieWrapper" id="trending" key={index}>
+                <Card className="movieImage">
+                  <Image src={`${process.env.REACT_APP_IMG_URL}/${result.poster_path}`} alt="test" className="images" />
+                  <div className="bg-dark">
+                    <div className="p-2 m-1 text-white">
+                      <Card.Title className="text-center">{result.title}</Card.Title>
+                      <Card.Text className="text-left">
+                        {result.overview}
+                      </Card.Text>
+                      <Card.Text className="text-left">
+                        {result.release_date}
+                      </Card.Text>
+                    </div>
+                  </div>
+                </Card>
+              </Col>
+            )
+          })}
+        </Row>
+      </Container>
+    </div>
+  )
 }
 
-export default Trending;
+export default Trending
